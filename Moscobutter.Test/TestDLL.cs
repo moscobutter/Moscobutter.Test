41689a27214f43867233289d5ed84d2bca0a8339
Moses Lebotsa <Moses_Lebotsa@sauonline.local>
2018-07-13 16:48:32 +0200

    first commit

---
+++ Moscobutter.Test/TestDLL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Moscobutter.Test
+{
+    public static class TestDLL
+    {
+        /// <summary>
+        /// Method to test nuget
+        /// </summary>
+        /// <returns></returns>
+        public static bool IsTest()
+        {
+            //return result
+            return true;
+        }
+    }
+}
